42c1d93141d35047c91a4c38cf60845014254975
Pawan Uikey <pawanuikey690@gmail.com>
2024-12-12 17:43:00 +0530

    '/hello' Route and Homepage is   created

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
+
