4f9965fbb3f522017385dc49c1dfb110eedc4ab7
Pawan Uikey <pawanuikey690@gmail.com>
2024-12-12 18:25:53 +0530

    implementd '/users' ROUTE

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -9,5 +10,35 @@ def index():
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
+
+
+
+
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',        
+    'password': 'Uikey@06',
+    'database': 'users'    
+}
+@app.route('/users')
+def users():
+    
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, name, email, role FROM users")
+    users_list = cursor.fetchall()
+
+   
+    cursor.close()
+    conn.close()
+
+    
+    return render_template('users.html', users=users_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
